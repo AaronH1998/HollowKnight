3e52a020f2bece685253aba991860a635cc5c849
Aaron Hodgson <aaronhodgson1998@gmail.com>
2024-03-05 00:19:02 +0000

    Added main menu and music

--- scenes/menus/game_complete.gd
+++ scenes/menus/game_complete.gd
@@ -4,7 +4,7 @@ var can_continue: bool = false
 
 func _input(event):
 	if can_continue and (event is InputEventKey or event is InputEventMouseButton) and event.pressed:
-		TransitionLayer.change_scene("res://scenes/levels/level_one.tscn")
+		TransitionLayer.change_scene("res://scenes/menus/main_menu.tscn")
 
 func allow_continue():
 	can_continue = true
---
+++ scenes/menus/main_menu.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+func _on_start_game_button_pressed():
+	TransitionLayer.change_scene("res://scenes/levels/level_one.tscn")
+
+
+func _on_button_pressed():
+	print("LMAO CUNT")
