eab818170e512b8ea6cfe52d11c6185a3cddf2e0
Aaron Hodgson <aaronhodgson1998@gmail.com>
2024-06-23 19:26:17 +0100

    wired up exit to main menu button and hide mouse on unpause

--- globals/transition_layer.gd
+++ globals/transition_layer.gd
@@ -4,4 +4,5 @@ func change_scene(target: String) -> void:
 	$AnimationPlayer.play("fade_to_black")
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file.bind(target).call_deferred()
+	get_tree().paused = false
 	$AnimationPlayer.play_backwards("fade_to_black")
--- scenes/UI/pause_menu.gd
+++ scenes/UI/pause_menu.gd
@@ -1,30 +1,39 @@
 extends CanvasLayer
 
 @onready var resume_button: HBoxContainer = $VBoxContainer/VBoxContainer/ResumeButton
+
 var paused: bool = false
+
 
 func _input(_event):
 	if Input.is_action_just_pressed("menu"):
-		get_tree().paused = !paused
 		paused = !paused
-		
 		if paused:
 			_pause()
 		else:
-			hide()
+			_unpause()
+
 
 func _ready():
 	hide()
-	resume_button.focus()
-	Globals.connect("paused", _pause)
 
 
 func _pause():
+	get_tree().paused = true
 	show()
 	resume_button.focus()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
-func _on_resume_button_pressed():
+func _unpause():
 	hide()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	get_tree().paused = false
+
+
+func _on_resume_button_pressed():
+	_unpause()
+
+
+func _on_main_menu_button_pressed():
+	TransitionLayer.change_scene("res://scenes/menus/main_menu.tscn")
